84249fe2fa13fbe3221e15b2123b6799b08be0ab
despoina <dntakou@upgrade.net.gr>
2023-06-09 18:42:47 +0300

    the other 2 tiers

---
+++ tier2_question3/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Task.tier2_question3
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+
+            //creation of a random number's List 
+            List<int> numbers = new List<int> { 2, 3, -9, 10, 15, -7, 6, 0 };
+
+            //initialization of sum which adds the numbers that are divided by 3 and also turned into their absolute value through Math.Abs()
+            int sum = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num % 3 == 0)
+                {
+                    sum += Math.Abs(num);
+                }
+            }
+
+            Console.WriteLine("Sum of absolute values divisible by 3: " + sum);
+        }
+    }
+}
---
+++ tier3_question2/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace C_Task.tier3_question2
+{
+   public class Class1
+    {
+        static void Main(string[] args)
+        {
+
+            //we create a List of strings to check which of those are anagrams
+            List<string> strings = new List<string> { "listen", "silent", "elbow", "below", "state", "taste" };
+
+
+
+            List<List<string>> anagramGroups = GetAnagramGroups(strings);
+
+            foreach (List<string> group in anagramGroups)
+            {
+                if (group.Count > 1)
+                {
+                    Console.WriteLine("Anagram Group: " + string.Join(", ", group));
+                }
+            }
+        }
+
+        static List<List<string>> GetAnagramGroups(List<string> strings)
+        {
+            Dictionary<string, List<string>> anagramMap = new Dictionary<string, List<string>>();
+
+            foreach (string str in strings)
+            {
+                char[] charArray = str.ToCharArray();
+                Array.Sort(charArray);
+                string sortedStr = new string(charArray);
+
+                if (!anagramMap.ContainsKey(sortedStr))
+                {
+                    anagramMap[sortedStr] = new List<string>();
+                }
+
+                anagramMap[sortedStr].Add(str);
+            }
+
+            return anagramMap.Values.ToList();
+        }
+    }
+
+}
+
